a37b986137c1ad072e3a495d2789852c9f027e8e
AlexBrown13 <rupcgroup25.6@gmail.com>
2026-04-29 14:09:46 +0300

    Add cron job in GitHub actions

--- server/services/google_trends.py
+++ server/services/google_trends.py
@@ -1,18 +1,50 @@
+import time
+import datetime
+import pandas as pd
+import logging
+import logging.handlers
+
 from pytrends.request import TrendReq
 from mongo import trends_collection
 from pymongo import UpdateOne
-import time
-import pandas as pd
+from pytrends.exceptions import TooManyRequestsError
+
+logger = logging.getLogger("google_trends")
+logger.setLevel(logging.INFO)
+
+LOG_FILE = "status.log"
+
+file_handler = logging.handlers.RotatingFileHandler(
+    LOG_FILE,
+    maxBytes=1024 * 1024,  # 1MB
+    backupCount=3,
+    encoding="utf8",
+)
+
+formatter = logging.Formatter(
+    "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+
+file_handler.setFormatter(formatter)
+
+if not logger.handlers:
+    logger.addHandler(file_handler)
+
 
 def main():
-    
-    index_keywords_HE = ['טראומה', 'לחץ נפשי', 'חשש', 'חרדה',]
-    index_keywords_EN = ['Trauma', 'Stress','Fear','Panic_Disorder']
+    logger.info("Trnds job started")
+
+    # index_keywords_HE = ['טראומה', 'לחץ נפשי', 'חשש', 'חרדה',]
+    # index_keywords_EN = ['Trauma', 'Stress','Fear','Panic_Disorder']
+
+    # groups = {
+    #     "Trauma_Index": ["טראומה",], # ["טראומה" ,"פוסט טראומה", "PTSD"]
+    #     "Anxiety_Index": ["חרדה",],  # ["חרדה", "לחץ נפשי", "דאגה"]
+    #     "Fear_Index": ["חשש",]       # ["חשש", "פחד", "אזעקות"] 
+    # }
 
     groups = {
-        "Trauma_Index": ["טראומה",], # ["טראומה" ,"פוסט טראומה", "PTSD"]
-        "Anxiety_Index": ["חרדה",],  # ["חרדה", "לחץ נפשי", "דאגה"]
-        "Fear_Index": ["חשש",]       # ["חשש", "פחד", "אזעקות"] 
+        "Trauma_Index": ["טראומה", "PTSD"],
     }
 
     try:
@@ -20,6 +52,7 @@ def main():
         final_df = pd.DataFrame()
         
         for index, (group_name, kw_list) in enumerate(groups.items()):
+            logger.info(f"Processing group: {group_name}")
             print(f"Fetching {group_name}...")
 
             pytrends.build_payload(
@@ -47,12 +80,17 @@ def main():
             # avoid 429 error
             if index < len(groups)-1:
                 time.sleep(60)
-            
+
+        logger.info("fetched data successfully ")
         return final_df
         
-    except Exception as e:
+    except TooManyRequestsError as e:
+        logger.error(f"{group_name}: Google returned 429 (rate limited)")
         print(f"Error in Google trends: {e}")
         exit()
+
+    except Exception as e:
+        logger.exception(f"Unexpected error in {group_name}: {e}")
 
 
 def update_database(df_new):
@@ -66,8 +104,10 @@ def update_database(df_new):
             response = trends_collection.insert_many(records)
 
             if response.acknowledged:
+                logger.info("Initial data inserted into database successfully")
                 print("Initial data inserted successfully")
             else:
+                logger.error("Failed to insert initial data into database")
                 print("Failed to insert initial data")
 
         else:
@@ -83,9 +123,11 @@ def update_database(df_new):
 
             if operations:
                 trends_collection.bulk_write(operations)
+                logger.info("Existing data updated successfully in the database")
                 print("Existing data updated successfully")
        
     except Exception as e:
+        logger.error("Error saving data to MongoDB")
         print(f"Error saving data to MongoDB {e}")
         exit()
 
