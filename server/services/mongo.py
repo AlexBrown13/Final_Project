20bf95c57cc7b06e69914b9f2448a9b17d87e9ac
AlexBrown13 <rupcgroup25.6@gmail.com>
2026-05-11 22:18:59 +0300

    update job

--- server/services/mongo.py
+++ server/services/mongo.py
@@ -3,8 +3,8 @@ from pymongo import MongoClient
 from dotenv import load_dotenv
 load_dotenv()
 
-mongo_url = os.environ["MONGO_ATLAS_URL"]
-db_name = os.environ.get("DB_ATLAS_NAME")
+mongo_url = os.environ["MONGO_URL"] #MONGO_ATLAS_URL
+db_name = os.environ.get("DB_NAME") #DB_ATLAS_NAME
 
 if not mongo_url:
     raise ValueError("Missing MONGO_URL environment variable")
